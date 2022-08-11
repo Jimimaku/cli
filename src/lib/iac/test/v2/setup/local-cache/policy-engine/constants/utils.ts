5b3c1c0f2d7db351c2c0d1231e71e8ddee6c2204
Francesco Mari <francesco.mari@snyk.io>
2022-08-11 11:03:45 +0200

    refactor: derive the Policy Engine version from the checksums

--- src/lib/iac/test/v2/setup/local-cache/policy-engine/constants/utils.ts
+++ src/lib/iac/test/v2/setup/local-cache/policy-engine/constants/utils.ts
@@ -1,5 +1,17 @@
 import * as os from 'os';
 
+// this const is not placed in `index.ts` to avoid circular dependencies
+const policyEngineChecksums = `
+0a2566f393a963c4a863e774fb4fa055ecc5fd018407f4c92213085592693eb9  snyk-iac-test_0.15.0_Linux_x86_64
+5d46d756f849704cd811707e552dd9a9e047b01ea6ea43ce424ddd41936b295f  snyk-iac-test_0.15.0_Linux_arm64
+7c2e881766c26711cd51f5027ff0f84ffae02d419a6489fd219092e5a2b3253d  snyk-iac-test_0.15.0_Windows_x86_64.exe
+8e1f0c5ba1e0c4b3344218e1373b81a48c25f7d58e755e3032b7f995e4f0a6f8  snyk-iac-test_0.15.0_Windows_arm64.exe
+c456b9e7b0eb9c73e406cb955e5dfb6b0adc3cee92b3a17c23ec5f23f37f9eb4  snyk-iac-test_0.15.0_Darwin_x86_64
+d6d6e9b0f722b125e7be5e21a03104babad6949bb7ca6f7e2b19cc044fda9169  snyk-iac-test_0.15.0_Darwin_arm64
+`;
+
+export const policyEngineVersion = getPolicyEngineVersion();
+
 export function formatPolicyEngineFileName(releaseVersion: string) {
   let platform = 'Linux';
   switch (os.platform()) {
@@ -17,15 +29,6 @@ export function formatPolicyEngineFileName(releaseVersion: string) {
 
   return `snyk-iac-test_${releaseVersion}_${platform}_${arch}${execExt}`;
 }
-
-// this const is not placed in `index.ts` to avoid circular dependencies
-const policyEngineChecksums = `0a2566f393a963c4a863e774fb4fa055ecc5fd018407f4c92213085592693eb9  snyk-iac-test_0.15.0_Linux_x86_64
-5d46d756f849704cd811707e552dd9a9e047b01ea6ea43ce424ddd41936b295f  snyk-iac-test_0.15.0_Linux_arm64
-7c2e881766c26711cd51f5027ff0f84ffae02d419a6489fd219092e5a2b3253d  snyk-iac-test_0.15.0_Windows_x86_64.exe
-8e1f0c5ba1e0c4b3344218e1373b81a48c25f7d58e755e3032b7f995e4f0a6f8  snyk-iac-test_0.15.0_Windows_arm64.exe
-c456b9e7b0eb9c73e406cb955e5dfb6b0adc3cee92b3a17c23ec5f23f37f9eb4  snyk-iac-test_0.15.0_Darwin_x86_64
-d6d6e9b0f722b125e7be5e21a03104babad6949bb7ca6f7e2b19cc044fda9169  snyk-iac-test_0.15.0_Darwin_arm64
-`;
 
 export function getChecksum(policyEngineFileName: string): string {
   const lines = policyEngineChecksums.split(/\r?\n/);
@@ -47,4 +50,32 @@ export function getChecksum(policyEngineFileName: string): string {
   }
 
   return policyEngineChecksum;
+}
+
+function getPolicyEngineVersion(): string {
+  const lines = policyEngineChecksums.split('\n');
+
+  if (lines.length == 0) {
+    throw new Error('empty checksum');
+  }
+
+  const line = lines.find((line) => line.length > 0);
+
+  if (line === undefined) {
+    throw new Error('empty checksum lines');
+  }
+
+  const parts = line.split(/\s+/);
+
+  if (parts.length < 2) {
+    throw new Error('invalid checksum line');
+  }
+
+  const components = parts[1].split('_');
+
+  if (components.length < 2) {
+    throw new Error('invalid checksum file name');
+  }
+
+  return components[1];
 }
